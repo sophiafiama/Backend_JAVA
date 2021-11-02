437d98b831897a8901de75685584adfc263913d4
sophiafiama <fiama.sophia.sf@gmail.com>
2021-11-02 17:37:54 -0300

    atualizando exercicios de aula

--- src/Aula0010_Logging/Guiado/Main.java
+++ src/Aula0010_Logging/Guiado/Main.java
@@ -1,66 +1,38 @@
 package Aula0010_Logging.Guiado;
 
-import java.sql.*;
+
+import org.apache.log4j.Logger;
 
 public class Main {
-    private static final String SQL_CREATE_TABLE = "DROP TABLE IF EXISTS ANIMAIS; " +
-            "CREATE TABLE ANIMAIS(" +
-            "ID INT PRIMARY KEY," +
-            "NOME varchar(100) NOT NULL," +
-            "TIPO varchar(100) NOT NULL)";
 
-    private static final String SQL_INSERT = "INSERT INTO ANIMAIS (ID, NOME, TIPO) VALUES (1, 'Pancho', 'Cachorro')";
-    private static final String SQL_INSERT2 = "INSERT INTO ANIMAIS (ID,NOME, TIPO) VALUES (2,'Pillo', 'Gato')";
-    private static final String SQL_INSERT3 = "INSERT INTO ANIMAIS (ID,NOME, TIPO) VALUES (3,'Cepillo', 'Elefante')";
-    private static final String SQL_INSERT4 = "INSERT INTO ANIMAIS (ID,NOME, TIPO) VALUES (4,'Pepe', 'Cachorro')";
-    private static final String SQL_INSERT5 = "INSERT INTO ANIMAIS (ID,NOME, TIPO) VALUES (5,'Rolo', 'Cavalo')";
+    private static final Logger logger = Logger.getLogger(Main.class);
 
-    private static final String SQL_DELETE = "DELETE FROM ANIMAIS WHERE ID=4";
+    public static void main(String[] args) {
+        Leao leao = new Leao();
+        leao.setIdade(4);
+        leao.setNome("Simba");
+        leao.seteAlfa(true);
+        leao.correr();
 
-    public static void main(String[] args) throws Exception {
-        Connection connection = null;
 
         try {
-            connection = getConnection();
-            Statement statement = connection.createStatement();
-            statement.execute(SQL_CREATE_TABLE);
-
-            Statement statement1 = connection.createStatement();
-            statement1.execute(SQL_INSERT);
-            //Statement statement = connection.createStatement();
-            statement.execute(SQL_INSERT2);
-            //Statement statement = connection.createStatement();
-            statement.execute(SQL_INSERT3);
-            //Statement statement = connection.createStatement();
-            statement.execute(SQL_INSERT4);
-            //Statement statement = connection.createStatement();
-            statement.execute(SQL_INSERT5);
-
-            verAnimais(connection);
-
-            statement.execute(SQL_DELETE);
-
-            verAnimais(connection);
-
+            leao.eMaiorQue10();
         } catch (Exception e){
-            e.printStackTrace();
-        } finally {
-            connection.close();
+            logger.error("A idade do leão "+leao.getNome()+" está incorreta", e);
         }
-    }
 
-    public static Connection getConnection() throws Exception {
-        Class.forName("org.h2.Driver").newInstance();
-        return DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
-    }
+        Leao leao2 = new Leao();
+        leao2.setIdade(-1);
+        leao2.setNome("Mufasa");
+        leao2.seteAlfa(false);
+        leao2.correr();
 
-    private static void verAnimais(Connection connection) throws SQLException {
-        String sql = "SELECT * FROM ANIMAIS";
-        Statement sqlSmt = connection.createStatement();
-        ResultSet rs = sqlSmt.executeQuery(sql);
-        while(rs.next()) {
-            System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+
+        try {
+            leao2.eMaiorQue10();
+        } catch (Exception e){
+            logger.error("A idade do leão "+leao2.getNome()+" está incorreta", e);
         }
-        System.out.println("------------------------------------------------------------------");
+
     }
 }
